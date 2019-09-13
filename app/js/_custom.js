f09f636948978a6511d979ad300a1d838416a668
AlexHuts <vatslav.zoltan@gmail.com>
2019-09-13 17:15:53 +0300

    jq update

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -16,4 +16,25 @@ document.addEventListener("DOMContentLoaded", function() {
 		}, 500, 'linear');
 	});
 
+	
+
+});
+$(document).ready(function(){
+	//E-mail Ajax Send
+	$("form.callback").submit(function(e) { //Change
+		e.preventDefault(); 
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display','flex').hide().fadeIn();
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
 });
