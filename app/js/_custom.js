5108b1ac3eb1cc83211ca84c65039090f76246f4
vatslav.zoltan@gmail.com <vatslav.zoltan@gmail.com>
2019-09-15 13:43:52 +0300

    mobile menu fix

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,40 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-	$('.hamburger').on('click', function(e){
+  $(".hamburger").on("click", function(e) {
     e.preventDefault();
-    $(this).toggleClass('active');
-    $('.header').toggleClass('active');
-	});
-	
-	$('a[href*="#"]').on('click', function (e) {
-		e.preventDefault();
-		$('.page_nav').removeClass('active');
-		$('.hamburger').removeClass('active');
-	
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top-100
-		}, 500, 'linear');
-	});
+    $(this).toggleClass("active");
+    $(".header").toggleClass("active");
+  });
 
-	
+  $('a[href*="#"]').on("click", function(e) {
+    e.preventDefault();
+    $(".header").removeClass("active");
+    $(".hamburger").removeClass("active");
 
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100
+      },
+      500,
+      "linear"
+    );
+  });
 });
-$(document).ready(function(){
-	//E-mail Ajax Send
-	$("form.callback").submit(function(e) { //Change
-		e.preventDefault(); 
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			$(th).find('.success').addClass('active').css('display','flex').hide().fadeIn();
-			setTimeout(function() {
-				$(th).find('.success').removeClass('active').fadeOut();
-				th.trigger("reset");
-			}, 3000);
-		});
-		return false;
-	});
+$(document).ready(function() {
+  //E-mail Ajax Send
+  $("form.callback").submit(function(e) {
+    //Change
+    e.preventDefault();
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      $(th)
+        .find(".success")
+        .addClass("active")
+        .css("display", "flex")
+        .hide()
+        .fadeIn();
+      setTimeout(function() {
+        $(th)
+          .find(".success")
+          .removeClass("active")
+          .fadeOut();
+        th.trigger("reset");
+      }, 3000);
+    });
+    return false;
+  });
 });
